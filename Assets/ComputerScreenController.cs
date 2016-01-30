e05f8f51e1e8f9df0bd4795960385ebb7f0886d4
Ville Touronen <touronen@gmail.com>
2016-01-30 19:42:38 +0200

    Screen open and close with left ctrl

---
+++ Assets/ComputerScreenController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ComputerScreenController : MonoBehaviour {
+
+	private Vector3 originalScale;
+	private bool screenOpened = false; 
+
+	// Use this for initialization
+	void Start () {
+		this.originalScale = gameObject.transform.localScale;
+		this.closeScreen ();
+	}
+
+	void Update()
+	{
+		if (Input.GetButtonDown ("Fire1")) {
+			if (this.screenOpened) {
+				this.closeScreen ();	
+			} else {
+				this.openScreen ();
+			}
+		}
+	}
+
+	void openScreen() {
+		this.screenOpened = true;
+		gameObject.transform.localScale = this.originalScale;
+	}
+
+	void closeScreen() {
+		this.screenOpened = false;
+		gameObject.transform.localScale = new Vector3(0, 0, 0);
+	}
+}
